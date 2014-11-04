c7a7d5fe093b24ec714e67a58910bff9fbb41f70
Jon Newton <jon.newton@gmail.com>
2014-11-03 20:00:21 -0700

    fressian basics

--- src/sequoia/durable.clj
+++ src/sequoia/durable.clj
@@ -1,5 +1,5 @@
 (ns sequoia.durable
-  (:import (java.io BufferedOutputStream ByteArrayOutputStream))
+  (:import (java.io BufferedOutputStream ByteArrayOutputStream ByteArrayInputStream))
   (:require [clojure.data.fressian :as f]))
 
 (def db0
@@ -7,7 +7,19 @@
             :occupants [:ben :jan :bentley]}})
 
 (def buf (f/write db0))
-
 (f/read buf)
 
-(f/create-writer (ByteArrayOutputStream. (* 1024 1024)))
+(def out (ByteArrayOutputStream. 10000))
+(def wrt (f/create-writer out))
+
+(f/write-object wrt db0)
+
+(eval out)
+
+(def rdr (f/create-reader (ByteArrayInputStream. (.toByteArray out))))
+(f/read-object rdr)
+
+;; By using the FileChannel for a FileInputStream, the channel's
+;; position methods can be used to control the position in the file.
+;; Both a read and write channel should be open at the same time.
+;; Reads could probably be multi-threaded with a reader per thread
